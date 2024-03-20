e9bd2b00d7f1dc1179043587403d41162c74f656
Winrar <pv23002@esfe.agape.edu.sv>
2024-03-20 09:14:33 -0600

    go go go

--- BibliotecaESFE.EN/Libros.cs
+++ BibliotecaESFE.EN/Libros.cs
@@ -29,5 +29,6 @@ namespace BibliotecaESFE.EN
         public Autores? Autores { get; set; }
         public Editoriales? Editoriales { get; set; }
         public Categorias? Categorias { get; set; }
+        public List<Reservas>? Reservas { get; set; }
     }
 }
